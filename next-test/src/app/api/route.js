75ad9e1c9fc95e36e3e2bded0689b23e8f2c0bbf
MattWong-ca <matthew.yt.wong@gmail.com>
2024-11-16 12:35:07 +0700

    test new API

---
+++ next-test/src/app/api/route.js
@@ -0,0 +1,21 @@
+export async function POST(req) {
+  try {
+    const data = await req.json();
+    console.log("Webhook Data:", data);
+
+    return new Response(JSON.stringify({ message: "Webhook received successfully!" }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error("Error handling webhook:", error);
+    return new Response(JSON.stringify({ message: "Server error" }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
